c55ffc13af6de593e5c7ae25040aca85b2052112
Paulo Sergio <paulo.sergio@tallos.com.br>
2023-08-17 21:28:45 -0300

    feat: criando instancias dos metodos

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,32 @@
 import express, { Application } from 'express';
+import { errorMiddleware } from './middlewares/error.middleware';
 
 export class App {
     public app: Application;
 
     constructor() {
         this.app = express();
+        this.middlewaresInitializer();
+        this.initRouter();
+        this.interceptors();
     }
 
-    listen(port: string) {
+    middlewaresInitializer() {
+        this.app.use(express.json());
+        this.app.use(express.urlencoded({ extended: true }));
+    }
+
+    interceptors() {
+        this.app.use(errorMiddleware);
+    }
+
+    initRouter() {
+        this.app.get('/', (req, res) => {
+            res.send('Hello bookhub')
+        })
+    }
+
+    listen(port: number) {
         this.app.listen(port, () => console.log(`server is running on port http://localhost:${port}`));
     }
 }
